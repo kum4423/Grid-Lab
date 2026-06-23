9b5fe9df8a676e57fe97347f121c3be04e15ebf8
Kum Hathaway <kumpei.suzuki@gmail.com>
2026-06-23 20:19:08 +0900

    Bug fix
    Short動画の表示がおかしい件について修正

--- content_script.js
+++ content_script.js
@@ -166,13 +166,13 @@ const optionShortsPerRow = (amount) => {
   addStyle(
     KeyShelfItemPerRow,
     `
-    [page-subtype="subscriptions"] ytd-rich-shelf-renderer[is-shorts] ytd-rich-item-renderer[is-slim-media],
-    [page-subtype="home"] ytd-rich-shelf-renderer[is-shorts] ytd-rich-item-renderer[is-slim-media] {
+    [page-subtype="home"] ytd-rich-shelf-renderer[is-shorts] ytd-rich-item-renderer[is-slim-media],
+    [page-subtype="subscriptions"] ytd-rich-shelf-renderer[is-shorts] ytd-rich-item-renderer[is-slim-media] {
       width: calc(100%/${amount} - var(--ytd-rich-grid-item-margin) - 0.01px) !important;
     }
-    [page-subtype="subscriptions"] ytd-rich-shelf-renderer[is-shorts] ytd-rich-item-renderer[is-slim-media],
-    [page-subtype="home"] ytd-rich-shelf-renderer[is-shorts] ytd-rich-item-renderer[is-slim-media]:nth-child(-n + ${amount}) {
-      display: block !important;
+    [page-subtype="home"] ytd-rich-shelf-renderer[is-shorts] ytd-rich-item-renderer[is-slim-media]:nth-child(n + ${amount + 1}),
+    [page-subtype="subscriptions"] ytd-rich-shelf-renderer[is-shorts] ytd-rich-item-renderer[is-slim-media]:nth-child(n + ${amount + 1}) {
+      display: none !important;
     }
   `
   );
